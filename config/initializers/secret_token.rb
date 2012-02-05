dca33ab4377503518dafe00c7c9c29567638e29e
okeen <eneko.taberna@gmail.com>
2012-02-05 21:30:01 +0100

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globelingua::Application.config.secret_token = 'c7946d81d5253db77afa3f87d6fc58b29601ba08c1402f9a7776d160a84680445904ba3b2db4a43dcca695acfc2f61044c6753a49c1457888e7be40f039f3c6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Globelingua::Application.config.session_store :cookie_store, key: '_globelingua_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Globelingua::Application.config.session_store :active_record_store
